ece7266f5301a9e53d7a7c7281c133468f433e1c
liu haixiang <haixiang1977@yahoo.com>
2018-05-01 21:29:07 +0800

    understanding future async ...
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

---
+++ test42/test42.cpp
@@ -0,0 +1,12 @@
+/*
+ * test42.cpp
+ *
+ *  Created on: May 1, 2018
+ *      Author: liu
+ */
+
+// http://en.cppreference.com/w/cpp/thread/future
+// http://www.cnblogs.com/haippy/p/3239248.html
+// https://blog.csdn.net/liuxuejiang158blog/article/details/17354115
+
+
