63f3af0aae47c12de30c7a5902f8adae2486a816
liu haixiang <haixiang1977@yahoo.com>
2018-05-01 20:23:47 +0800

    add std::function()
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

--- test40/test40.cpp
+++ test40/test40.cpp
@@ -1,1 +1,22 @@
 // https://blog.csdn.net/liukang325/article/details/53668046
+
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+void print_hello() {
+	cout << "hello std::function" << endl;
+
+	return;
+}
+
+int main() {
+	std::function<void()> func;
+
+	func = print_hello;
+
+	func();
+
+	return 0;
+}
