bc35094ab092a7cf71cd4d85a88a7566c4ba24fb
LIU YIYANG : ) <haixiang1977@users.noreply.github.com>
2021-03-04 12:11:57 +0800

    Update client.cpp

--- nonblock_send_receive/client.cpp
+++ nonblock_send_receive/client.cpp
@@ -15,39 +15,39 @@
 
 int main ()
 {
-	int sockfd; /* socket fd */
-	struct sockaddr_in servaddr;
-	char buf[256];
-	int num_read = 0;
-	int num_written = 0;
+    int sockfd; /* socket fd */
+    struct sockaddr_in servaddr;
+    char buf[256];
+    int num_read = 0;
+    int num_written = 0;
 
-	/* 1. create socket */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); /* IPV4 TCP Socket */
-	if (sockfd < 0) {
-		printf ("client failed to create TCP socket\n");
-		goto err;
-	}
+    /* 1. create socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); /* IPV4 TCP Socket */
+    if (sockfd < 0) {
+        printf ("client failed to create TCP socket\n");
+        goto err;
+    }
 
-	/* 2. prepare server information */
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons (5000);
+    /* 2. prepare server information */
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons (5000);
 
-	/* convert 127.0.0.1 into servaddr */
-	if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr.s_addr) < 0) { /* see man inet_pton */
-		printf ("client failed to inet_pton 127.0.0.1 \n");
-		goto err;
-	}
+    /* convert 127.0.0.1 into servaddr */
+    if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr.s_addr) < 0) { /* see man inet_pton */
+        printf ("client failed to inet_pton 127.0.0.1 \n");
+        goto err;
+    }
 
-	/* 3. connect to the server */
-	if (connect (sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-		printf ("client failed to connect the server\n");
-	} else {
-		printf ("client connected to the server\n");
-	}
+    /* 3. connect to the server */
+    if (connect (sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        printf ("client failed to connect the server\n");
+    } else {
+        printf ("client connected to the server\n");
+    }
 
-	/* 4. send message to the server with non-block mode */
-	strcpy (buf, "hello from client\n");
+    /* 4. send message to the server with non-block mode */
+    strcpy (buf, "hello from client\n");
     /* check socket is writable */
     {
         struct timeval timeout; // write timeout is 1sec
@@ -89,15 +89,15 @@ int main ()
             }
         } while (bytes_sent < total_length);
     }
-	
-	printf ("client num_written %d\n", num_written);
-	while (num_read == 0) {
-		num_read = read (sockfd, buf, sizeof(buf));
-	}
-	printf ("client num_read %d\n", num_read);
-	printf ("client received %s", buf);
+    
+    printf ("client num_written %d\n", num_written);
+    while (num_read == 0) {
+        num_read = read (sockfd, buf, sizeof(buf));
+    }
+    printf ("client num_read %d\n", num_read);
+    printf ("client received %s", buf);
 
-	return 0;
+    return 0;
 err:
-	return -1;
+    return -1;
 }
